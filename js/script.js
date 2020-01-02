b8f980f30f6166ea5e653878d5fcbec28c84deda
CabecasDeGelo <59445188+CabecasDeGelo@users.noreply.github.com>
2020-01-02 11:46:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,166 @@
+
+
+function iniciar(){
+    let nivel = document.getElementById("nivel").value
+    window.location.href = 'html/jogo.html?' + nivel;
+    window.alert('3...2...1... QUEBRE O MÁXIMO DE CUS POSSÍVEIS!')
+}
+
+
+
+
+var timerId = null; //variável que armazena a chamada da função timeout
+
+function iniciaJogo(){
+
+	var url = window.location.search;
+	
+	var nivel_jogo = url.replace("?", "");
+
+	var tempo_segundos = 0;
+
+	if(nivel_jogo == 1) { //1 fácil -> 120segundos
+		tempo_segundos = 120;
+	}
+
+	if(nivel_jogo == 2) { //2 normal -> 60segundos
+		tempo_segundos = 60;
+	}
+	
+	if(nivel_jogo == 3) { //3 difícil -> 30segundos
+		tempo_segundos = 30;
+	}
+
+	//inserindo segundos no span
+	document.getElementById('cronometro').innerHTML = tempo_segundos;
+
+	// quantidade de balões
+	var qtde_baloes = 80;
+	
+	cria_baloes(qtde_baloes);
+
+	//imprimir qtde baloes inteiros
+	document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
+	document.getElementById('baloes_estourados').innerHTML = 0;
+
+	contagem_tempo(tempo_segundos + 1)
+	
+}
+
+function contagem_tempo(segundos){
+
+	segundos = segundos - 1;
+
+	if(segundos == -1){
+		clearTimeout(timerId); //para a execução da função do settimeout
+		game_over();
+		return false;
+	}
+
+	document.getElementById('cronometro').innerHTML = segundos;
+
+	timerId = setTimeout("contagem_tempo("+segundos+")", 1000);
+}
+
+function game_over(){
+  
+	alert('Game over! Você foi diagnosticado com a Síndrome da Porra do Fracassado');
+  window.open('derrota.html', "_self");
+}
+
+function cria_baloes(qtde_baloes){
+
+	for(var i = 1; i <= qtde_baloes; i++){
+
+		var balao = document.createElement("img");
+		balao.src = '../imagens/jorjim_feliz.png';
+    balao.style.margin = '10px';
+    balao.style.cursor = 'pointer';
+		balao.id = 'b'+i;
+		balao.onclick = function(){ estourar(this); };
+
+		document.getElementById('cenario').appendChild(balao);
+	}
+}
+
+function estourar(e){
+
+	var id_balao = e.id;
+
+	document.getElementById(id_balao).src = '../imagens/jorjim_triste.png';
+
+	pontuacao(-1);
+
+}
+
+function pontuacao(acao){
+
+	var baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
+	var baloes_estourados  = document.getElementById('baloes_estourados').innerHTML;
+
+	baloes_inteiros = parseInt(baloes_inteiros);
+	baloes_estourados = parseInt(baloes_estourados);
+
+	baloes_inteiros = baloes_inteiros + acao;
+	baloes_estourados = baloes_estourados - acao;
+
+	document.getElementById('baloes_inteiros').innerHTML = baloes_inteiros;
+	document.getElementById('baloes_estourados').innerHTML = baloes_estourados;
+
+	situacao_jogo(baloes_inteiros);
+
+
+}
+
+function situacao_jogo(baloes_inteiros){
+	if(baloes_inteiros == 0){
+    window.open('vitoria.html', '_self');
+		alert('Parabéns, você quebrou o cu do Jorjasso. A Terra agora descansa em paz, sem o tarado da meia por perto.');
+		parar_jogo();
+	}
+}
+
+function parar_jogo(){
+	clearTimeout(timerId);
+}
+
+
+
+
+
+
+/* Get the element you want displayed in fullscreen */ 
+var elem = document.documentElement;
+
+/* Function to open fullscreen mode */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem = window.top.document.body; //To break out of frame in IE
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Function to close fullscreen mode */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    window.top.document.msExitFullscreen();
+  }
+}
+
+
+document.getElementById("fullscreen").addEventListener("click",() => {    
+    openFullscreen();
+    closeFullscreen();    
+});
